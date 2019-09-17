0308541ad97d29e870f9214f3239e42c6c3a0b1c
MadisonViaLux <madison.s.mckown@gmail.com>
2019-09-17 16:14:40 -0700

    9/17/2019

---
+++ friends/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import Login from './components/Login'
+import {FriendsList} from './components/FriendsList'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Route exact path='/'>
+          <Link to='/api/login'>Login</Link>
+        </Route>
+        
+        <Route exact path='friends' component={FriendsList} />
+        
+        <Route exact path='/api/login' component={Login} />
+        
+        hello there
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FriendsList = props => {
+    return(
+        <div>
+            HeLlO tHeRe
+        </div>
+    )
+}
